a6d52bcc637dbf5412e6e6fc83a11060f2bc283d
ngoakor12 <ngoakor12@gmail.com>
2026-06-09 12:23:53 +0200

    added images and more styling

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello world")
+// if freelance option is selected
+// - show the budget fields
+// else if general option is selected
+// - hide budget fields
+
+const selectField = document.getElementById("form-subject");
+const freelanceField = document.getElementsByClassName("freelance-field")[0];
+
+selectField.addEventListener("change", () => {
+  if (selectField.value === "freelance") {
+    freelanceField.style.display = "flex";
+  } else {
+    freelanceField.style.display = "none";
+  }
+});
